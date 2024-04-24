11e7bd4093d00d49465f745af517528c0bd77ed0
Rafael <rmartin65@gmail.com>
2024-04-24 13:47:25 +0100

    files update

--- Ejercicio 8- Fotos/index.js
+++ Ejercicio 8- Fotos/index.js
@@ -1,6 +1,18 @@
 function cargafoto(){
     document.getElementById('foto').src = 
     window.URL.createObjectURL(document.getElementById("imagen_fichero").files[0]);
-    document.getElementById('nombre_foto').innerHTML = 
-    document.getElementById("imagen_fichero").files[0].name;
+    // document.getElementById('nombre_foto').innerHTML = document.getElementById("imagen_fichero").files[0].name;
 }
+
+document.getElementById("formulario").addEventListener("submit",(event) =>{
+    event.preventDefault();
+    const formData = new FormData(formulario)
+    for (let pair of formData.entries()) {
+        console.log(pair[0]+ ' : ' + pair[1]); 
+    }
+    fetch("upload.php")
+    .then(response => response.json())
+    .then(datos => {
+        console.log(datos);
+    })
+})
